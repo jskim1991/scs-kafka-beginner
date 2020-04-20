671995899b53c46e64e134b534688dae626b31b4
JunHyunny <kang3966@naver.com>
2020-04-21 06:45:36 +0900

    2020-04-21 partitioned producer import optimize

--- 03-PartitionedProducer/src/main/java/io/nerdybros/course/channel/BindingChannels.java
+++ 03-PartitionedProducer/src/main/java/io/nerdybros/course/channel/BindingChannels.java
@@ -1,9 +1,7 @@
 package io.nerdybros.course.channel;
 
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 
 public interface BindingChannels {
 
